a9f6cac1b7ab5773caa37572ea6fb875550fea2b
Chris Gunning <chris@gunning.com>
2012-11-03 20:12:39 -0700

    Create model method in the Movie model to find movies whose directo matches that of the current movie.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,7 +14,8 @@ class MoviesController < ApplicationController
       flash[:notice] = "\'#{movie.title}\' has no director info."
       redirect_to movies_path
     end
-    @movies = Movie.where(["director = ?", @director])
+#    @movies = Movie.where(["director = ?", @director])
+    @movies = Movie.where_director_is @director
   end
 
   def index
